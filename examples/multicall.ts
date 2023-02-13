0ba04cacef22b53c0e9e1f1f9e01d8d82a16db5e
Pham Tu <tubackkhoa@gmail.com>
2023-02-13 14:48:49 +0700

    update sample

--- examples/multicall.ts
+++ examples/multicall.ts
@@ -15,28 +15,27 @@ import {
     client,
     'orai1yv8dnskhj427hd79xkk34zlvcyzkw7tve09ktp89jhr6x2r0rumsmnj07f'
   );
-
+  const addresses = [
+    'orai14n3tx8s5ftzhlxvq0w5962v60vd82h30rha573',
+    'orai1qdsj06kp9l92nekfxe5jmen34fz8zh86qtygca'
+  ];
   const res = await multicall.aggregate({
-    queries: [
-      {
-        address: 'orai1gwe4q8gme54wdk0gcrtsh4ykwvd7l9n3dxxas2',
-        data: toBinary({
-          balance: { address: 'orai14n3tx8s5ftzhlxvq0w5962v60vd82h30rha573' }
-        } as TokenQueryMsg)
-      },
-      {
-        address: 'orai1gwe4q8gme54wdk0gcrtsh4ykwvd7l9n3dxxas2',
-        data: toBinary({
-          balance: { address: 'orai1qdsj06kp9l92nekfxe5jmen34fz8zh86qtygca' }
-        } as TokenQueryMsg)
-      }
-    ]
+    queries: addresses.map((address) => ({
+      address: 'orai1gwe4q8gme54wdk0gcrtsh4ykwvd7l9n3dxxas2',
+      data: toBinary({
+        balance: { address }
+      } as TokenQueryMsg)
+    }))
   });
 
-  for (const data of res.return_data) {
-    if (data.success) {
-      const result = fromBinary(data.data) as BalanceResponse;
-      console.log(result.balance);
-    }
-  }
+  const result = Object.fromEntries(
+    addresses.map((address, ind) => {
+      const balanceRes = fromBinary(
+        res.return_data[ind].data
+      ) as BalanceResponse;
+      return [address, balanceRes.balance];
+    })
+  );
+
+  console.log(result);
 })();
