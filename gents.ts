7d227cc6be04ee050a3e3b16a61ab8fc4e921469
Pham Tu <tubackkhoa@gmail.com>
2023-04-15 14:19:22 +0700

    update new version

--- gents.ts
+++ gents.ts
@@ -1,11 +1,11 @@
-import codegen, { ContractFile } from '@cosmwasm/ts-codegen';
-import { TypescriptParser, File } from 'typescript-parser';
-import { join, basename, resolve as _resolve } from 'path';
+import codegen, { ContractFile } from '@cosmwasm/ts-codegen/packages/ts-codegen';
 import * as fs from 'fs';
+import { resolve as _resolve, basename, join } from 'path';
+import { File, TypescriptParser } from 'typescript-parser';
 
 const {
   existsSync,
-  promises: { readdir, readFile, writeFile, rm, mkdir }
+  promises: { readdir, readFile, writeFile, rm }
 } = fs;
 
 const genTS = async (contracts: Array<ContractFile>, outPath: string, enabledReactQuery: boolean = false) => {
@@ -47,38 +47,13 @@ const isPrivateType = (type: string) => {
   return type.endsWith('Response') || type === 'InstantiateMsg' || type === 'ExecuteMsg' || type === 'QueryMsg' || type === 'MigrateMsg';
 };
 
-const fixNested = async (clientName: string, ext: string, nestedResponses: { [key: string]: [string, string, string[]] }, outPath: string) => {
-  const clientFile = join(outPath, `${clientName}.${ext}`);
-  let clientData = (await readFile(clientFile)).toString();
-  Object.entries(nestedResponses).forEach(([key, [name, inputType]]) => {
-    clientData = clientData
-      .replace(`${name}: () => Promise<${key}>;`, `${name}: (input: ${inputType}) => Promise<${key}>;`)
-      .replace(`${name} = async (): Promise<${key}> => {`, `${name} = async (input:${inputType}): Promise<${key}> => {`)
-      .replace(`${name}: {}`, `${name}: input`);
-  });
-  await writeFile(clientFile, clientData);
-};
-
-const fixNestedReactQuery = async (clientName: string, ext: string, nestedResponses: { [key: string]: [string, string, string[]] }, outPath: string) => {
-  const clientFile = join(outPath, `${clientName}.${ext}`);
-  let clientData = (await readFile(clientFile)).toString();
-  Object.entries(nestedResponses).forEach(([key, [name, inputType]]) => {
-    clientData = clientData
-      .replace(`${inputType}<TData> extends ${clientName}ReactQuery<${key}, TData> {}`, `${inputType}<TData> extends ${clientName}ReactQuery<${key}, TData> {input: ${inputType}}`)
-      // use regular for dynamic replacement
-      .replace(new RegExp(`\\n\\}:\\s*([\\w_\\d]+${inputType})<TData>`), `,\n\tinput\n}: $1<TData>`)
-      .replace(`client.${name}()`, `client.${name}(input)`);
-  });
-  await writeFile(clientFile, clientData);
-};
-
-const fixImport = async (clientName: string, ext: string, typeData: { [key: string]: string }, nestedTypes: string[], outPath: string) => {
+const fixImport = async (clientName: string, ext: string, typeData: { [key: string]: string }, outPath: string) => {
   const clientFile = join(outPath, `${clientName}.${ext}`);
   const clientData = await readFile(clientFile);
 
   await writeFile(
     clientFile,
-    clientData.toString().replace(new RegExp(`import\\s+\\{(.*?)\\}\\s+from\\s+"\\.\\/${clientName}\\.types";`), (_, g1) => {
+    clientData.toString().replace(new RegExp(`import\\s+\\{(.*?)\\}\\s+from\\s+"\\.\\/${clientName}\\.types";`), (_, g1: string) => {
       const [clientImportData, typesImportData] = g1
         .trim()
         .split(/\s*,\s*/)
@@ -90,7 +65,7 @@ const fixImport = async (clientName: string, ext: string, typeData: { [key: stri
           [[], []]
         );
 
-      return `import {${typesImportData.join(', ')}} from "./types";\nimport {${[...clientImportData, ...nestedTypes].join(', ')}} from "./${clientName}.types";`;
+      return `import {${typesImportData.join(', ')}} from "./types";\nimport {${clientImportData.join(', ')}} from "./${clientName}.types";`;
     })
   );
 };
@@ -133,17 +108,6 @@ const fixTs = async (outPath: string, enabledReactQuery = false) => {
         }
       }
 
-      // fix nested schema
-      const contractName = basename(dir, typeExt);
-      const nestedResponses = nestedMap[contractName];
-      const nestedTypes: string[] = [];
-      if (nestedResponses) {
-        Object.entries(nestedResponses).forEach(([key, value]) => {
-          nestedTypes.push(key);
-          modifiedTsData.push(`export type ${key} = ${value[2].join(' | ')};`);
-        });
-      }
-
       // import from types, and remove from client
       modifiedTsData.unshift(`import {${importData.join(', ')}} from "./types";`);
 
@@ -152,17 +116,10 @@ const fixTs = async (outPath: string, enabledReactQuery = false) => {
       // update client file
 
       const clientName = basename(dir, typeExt);
-      await fixImport(clientName, 'client.ts', typeData, nestedTypes, outPath);
-
-      if (nestedResponses) {
-        await fixNested(clientName, 'client.ts', nestedResponses, outPath);
-      }
+      await fixImport(clientName, 'client.ts', typeData, outPath);
 
       if (enabledReactQuery) {
-        await fixImport(clientName, 'react-query.ts', typeData, nestedTypes, outPath);
-        if (nestedResponses) {
-          await fixNestedReactQuery(clientName, 'react-query.ts', nestedResponses, outPath);
-        }
+        await fixImport(clientName, 'react-query.ts', typeData, outPath);
       }
     })
   );
@@ -176,57 +133,8 @@ const fixTs = async (outPath: string, enabledReactQuery = false) => {
   }
 };
 
-const fixNestedSchema = async (packagePath: string) => {
-  const cargoMatched = (await readFile(join(packagePath, 'Cargo.toml'))).toString().match(/name\s*=\s*"(.*?)"/);
-  if (!cargoMatched) return;
-  const schemaPath = join(packagePath, 'artifacts', 'schema');
-  const schemaFile = join(schemaPath, cargoMatched[1] + '.json');
-  // fallback to old version
-  if (!existsSync(schemaFile)) {
-    return;
-  }
-
-  const schemaJSON = JSON.parse((await readFile(schemaFile)).toString());
-  if (!schemaJSON.query.anyOf) return;
-  const responses = {};
-  let update = false;
-  schemaJSON.query.anyOf = schemaJSON.query.anyOf.map((item: any) => {
-    if (item.$ref) {
-      update = true;
-    }
-    const ref = item.$ref || item.properties[item.required[0]].$ref;
-    if (!ref) return item;
-    const matched = ref.match(/([A-Z][a-z]+)Query$/)[1];
-    const name = matched.toLowerCase();
-    const input = ref.split('/').pop();
-    const subResponses = schemaJSON.query.definitions[input].oneOf.map((item: any) => schemaJSON.responses[item.required[0]].title);
-
-    responses[`${matched}Response`] = [name, input, subResponses];
-
-    return item.$ref
-      ? {
-          type: 'object',
-          required: [name],
-          properties: {
-            [name]: item
-          },
-          additionalProperties: false
-        }
-      : item;
-  });
-
-  if (update) {
-    await writeFile(schemaFile, JSON.stringify(schemaJSON, null, 2));
-  }
-  return responses;
-};
-
 let enabledReactQuery = false;
 let tsFolder = _resolve(__dirname, 'build');
-
-const nestedMap: {
-  [key: string]: { [key: string]: [string, string, string[]] };
-} = {};
 
 (async () => {
   const packages: string[] = [];
@@ -253,16 +161,6 @@ const nestedMap: {
       const baseName = basename(packagePath);
       const schemaDir = join(packagePath, 'artifacts', 'schema');
       if (!existsSync(schemaDir)) return false;
-      // try fix nested schema if has
-      const responses = await fixNestedSchema(packagePath);
-      if (responses) {
-        nestedMap[
-          packagePath
-            .split('/')
-            .pop()!
-            .replace(/(^\w|_\w)/g, (m, g1) => g1.slice(-1).toUpperCase())
-        ] = responses;
-      }
 
       return {
         name: baseName.replace(/^.|_./g, (m) => m.slice(-1).toUpperCase()),
