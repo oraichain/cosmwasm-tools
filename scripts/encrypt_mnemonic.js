945115ae27f903dee60458d071c4f51ff18d4797
Pham Tu <tubackkhoa@gmail.com>
2023-06-08 14:45:15 +0700

    v0.0.12

--- scripts/encrypt_mnemonic.js
+++
@@ -1,9 +0,0 @@
-const readlineSync = require('readline-sync');
-const { encrypt } = require('../src/common');
-
-module.exports = async (argv) => {
-  const mnemonic = argv._[1] ? require('fs').readFileSync(argv._[1]).toString() : readlineSync.question('enter mnemonic:', { hideEchoBack: true });
-  const password = readlineSync.question('enter passphrase:', { hideEchoBack: true });
-
-  console.log(encrypt(password, mnemonic));
-};
