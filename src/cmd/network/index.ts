13d9d074e359e843355d2e60c0d68db71dbc9d21
ducphamle2 <ducphamle212@gmail.com>
2023-11-25 15:59:49 -0800

    chore: remove redundant file input option in network command

--- src/cmd/network/index.ts
+++ src/cmd/network/index.ts
@@ -1,13 +1,9 @@
 import { Argv } from "yargs";
-import fs from "fs";
 import statesyncCmd from "./cmd/statesync";
 
 export default (yargs: Argv) => {
   yargs
     .usage("usage: $0 network <command> [options]")
-    .config("file-input", (path) => {
-      return { input: fs.readFileSync(path).toString() };
-    })
     .command(
       "statesync",
       "Run a state sync of a cosmos-based network",
