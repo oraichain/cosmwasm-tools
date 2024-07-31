d2d5e1f1bb6e6bdb635d4151e20b4831cdb83af1
trung2891 <trungnv280201@gmail.com>
2024-07-31 12:02:00 +0700

    fix: use denom in gasPrice instead of prefix

--- src/cmd/wasm/cmd/instantiate.ts
+++ src/cmd/wasm/cmd/instantiate.ts
@@ -1,45 +1,52 @@
 // @ts-nocheck
 
-import * as cosmwasm from '@cosmjs/cosmwasm-stargate';
-import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
-import { GasPrice } from '@cosmjs/stargate';
-import { Argv } from 'yargs';
-import { decryptMnemonic } from '../../../common';
+import * as cosmwasm from "@cosmjs/cosmwasm-stargate";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { GasPrice } from "@cosmjs/stargate";
+import { Argv } from "yargs";
+import { decryptMnemonic } from "../../../common";
 
 export default async (yargs: Argv) => {
   const { argv } = yargs
 
-    .option('code-id', {
-      describe: 'the code id of the smart contract',
-      type: 'number'
+    .option("code-id", {
+      describe: "the code id of the smart contract",
+      type: "number",
     })
-    .option('label', {
-      describe: 'the label of smart contract',
-      type: 'string'
+    .option("label", {
+      describe: "the label of smart contract",
+      type: "string",
     })
-    .option('fees', {
-      describe: 'the transaction fees',
-      type: 'string'
+    .option("fees", {
+      describe: "the transaction fees",
+      type: "string",
     })
-    .option('amount', {
-      type: 'string'
+    .option("amount", {
+      type: "string",
     })
-    .option('admin', {
-      type: 'string'
+    .option("admin", {
+      type: "string",
     });
 
-  const mnemonic = argv.ENCRYPTED_MNEMONIC ? decryptMnemonic(argv.ENCRYPTED_MNEMONIC) : argv.MNEMONIC;
+  const mnemonic = argv.ENCRYPTED_MNEMONIC
+    ? decryptMnemonic(argv.ENCRYPTED_MNEMONIC)
+    : argv.MNEMONIC;
 
-  const prefix = process.env.PREFIX || 'orai';
+  const prefix = process.env.PREFIX || "orai";
+  const denom = process.env.DENOM || "orai";
   const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
-    prefix
+    prefix,
   });
   const [firstAccount] = await wallet.getAccounts();
 
-  const client = await cosmwasm.SigningCosmWasmClient.connectWithSigner(process.env.RPC_URL, wallet, {
-    gasPrice: GasPrice.fromString(`${process.env.GAS_PRICES}${prefix}`),
-    prefix
-  });
+  const client = await cosmwasm.SigningCosmWasmClient.connectWithSigner(
+    process.env.RPC_URL,
+    wallet,
+    {
+      gasPrice: GasPrice.fromString(`${process.env.GAS_PRICES}${denom}`),
+      prefix,
+    }
+  );
 
   const { codeId, label, admin = firstAccount.address } = argv;
 
@@ -47,11 +54,18 @@ export default async (yargs: Argv) => {
     // next instantiate code
     const input = JSON.parse(argv.input);
 
-    const res = await client.instantiate(firstAccount.address, codeId, input, label, 'auto', { admin: admin });
+    const res = await client.instantiate(
+      firstAccount.address,
+      codeId,
+      input,
+      label,
+      "auto",
+      { admin: admin }
+    );
 
     console.log(res.contractAddress);
     return res.contractAddress;
   } catch (error) {
-    console.log('error: ', error);
+    console.log("error: ", error);
   }
 };
--- src/cmd/wasm/cmd/migrate.ts
+++ src/cmd/wasm/cmd/migrate.ts
@@ -1,45 +1,58 @@
 // @ts-nocheck
-import * as cosmwasm from '@cosmjs/cosmwasm-stargate';
-import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
-import { GasPrice } from '@cosmjs/stargate';
-import { decryptMnemonic } from '../../../common';
-import { Argv } from 'yargs';
+import * as cosmwasm from "@cosmjs/cosmwasm-stargate";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { GasPrice } from "@cosmjs/stargate";
+import { decryptMnemonic } from "../../../common";
+import { Argv } from "yargs";
 
 export const migrate = async (argv: Argv) => {
   const [address] = argv._.slice(-1);
   const { codeId } = argv;
-  const prefix = process.env.PREFIX || 'orai';
-  const mnemonic = argv.ENCRYPTED_MNEMONIC ? decryptMnemonic(argv.ENCRYPTED_MNEMONIC) : argv.MNEMONIC;
+  const prefix = process.env.PREFIX || "orai";
+  const denom = process.env.DENOM || "orai";
+  const mnemonic = argv.ENCRYPTED_MNEMONIC
+    ? decryptMnemonic(argv.ENCRYPTED_MNEMONIC)
+    : argv.MNEMONIC;
   const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
-    prefix
+    prefix,
   });
   const [firstAccount] = await wallet.getAccounts();
-  const client = await cosmwasm.SigningCosmWasmClient.connectWithSigner(process.env.RPC_URL, wallet, {
-    gasPrice: GasPrice.fromString(`${process.env.GAS_PRICES}${prefix}`),
-    prefix
-  });
+  const client = await cosmwasm.SigningCosmWasmClient.connectWithSigner(
+    process.env.RPC_URL,
+    wallet,
+    {
+      gasPrice: GasPrice.fromString(`${process.env.GAS_PRICES}${denom}`),
+      prefix,
+    }
+  );
 
   try {
     // next instantiate code
     const input = JSON.parse(argv.input);
 
-    const res = await client.migrate(firstAccount.address, address, codeId, input, 'auto');
+    const res = await client.migrate(
+      firstAccount.address,
+      address,
+      codeId,
+      input,
+      "auto"
+    );
 
     console.log(res);
   } catch (error) {
-    console.log('error: ', error);
+    console.log("error: ", error);
   }
 };
 
 export default async (yargs: Argv) => {
   const { argv } = yargs
-    .positional('address', {
-      describe: 'the smart contract address',
-      type: 'string'
+    .positional("address", {
+      describe: "the smart contract address",
+      type: "string",
     })
-    .option('code-id', {
-      describe: 'the code id of the smart contract',
-      type: 'number'
+    .option("code-id", {
+      describe: "the code id of the smart contract",
+      type: "number",
     });
   await migrate(argv);
 };
